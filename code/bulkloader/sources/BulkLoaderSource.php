d6b727656672c0b726f496c7a6be477f81a40b05
botzkobg <tdenev@gmail.com>
2017-01-19 17:13:14 +0000

    Fix issue with php version 5.3.3

--- code/bulkloader/sources/BulkLoaderSource.php
+++ code/bulkloader/sources/BulkLoaderSource.php
@@ -1,6 +1,6 @@
 <?php
 
-if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+
 
 	/**
 	 * An abstract source to bulk load records from.
@@ -12,25 +12,3 @@ if (version_compare(PHP_VERSION, '5.4.0', '<')) {
 	{
 
 	}
-
-} else {
-
-	/**
-	 * An abstract source to bulk load records from.
-	 * Provides an iterator for retrieving records from.
-	 *
-	 * Useful for holiding source configuration state.
-	 */
-	abstract class BulkLoaderSource implements IteratorAggregate
-	{
-
-		/**
-		 * Provide iterator for bulk loading from.
-		 * Records are expected to be 1 dimensional key-value arrays.
-		 * @return Iterator
-		 */
-		abstract public function getIterator();
-
-	}
-
-}

baa6b295f4f51d8f3e5f0ce3f26413e7bc9fe9b6
BLMaciej <maciej@bookinglive.com>
2020-05-19 18:25:09 +0100

    Update ExportAdminExtension.php

--- code/extensions/ExportAdminExtension.php
+++ code/extensions/ExportAdminExtension.php
@@ -4,13 +4,14 @@ class ExportAdminExtension extends Extension {
     public function updateEditForm($form){
 		$gridField = $form->Fields()->fieldByName($this->owner->modelClass);
         $config = $gridField->getConfig();
-        $oldExportButton = $config->getComponentByType('GridFieldExportButton');
-        $config->addComponent($newExportButton = new GridFieldExporter('before'));
-        
-        // Set Header and Export columns on new Export Button
-        $newExportButton->setCsvHasHeader($oldExportButton->getCsvHasHeader()); 
-        $newExportButton->setExportColumns($oldExportButton->getExportColumns());
-        
-        $config->removeComponentsByType('GridFieldExportButton');
+        if($oldExportButton = $config->getComponentByType('GridFieldExportButton')) {
+		$config->addComponent($newExportButton = new GridFieldExporter('before'));
+
+		// Set Header and Export columns on new Export Button
+		$newExportButton->setCsvHasHeader($oldExportButton->getCsvHasHeader()); 
+		$newExportButton->setExportColumns($oldExportButton->getExportColumns());
+
+		$config->removeComponentsByType('GridFieldExportButton');
+	}
     }
 }
